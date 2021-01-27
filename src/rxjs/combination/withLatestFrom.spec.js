01a0e5871aac359aee8e8409c2b7469bf7c0b3c9
Alex Zagorodnik <olexandr.zagorodnik@gmail.com>
2021-01-27 06:03:01 +0200

    add withLatestFrom test

---
+++ src/rxjs/combination/withLatestFrom.spec.js
@@ -0,0 +1,14 @@
+const { withLatestFrom, take } = require('rxjs/operators');
+const { interval } = require('rxjs');
+
+const example = interval(250).pipe(
+    take(3),
+    withLatestFrom(interval(350).pipe(take(3)))
+)
+
+test('test withLatestFrom', done => {
+    example.subscribe(val => {
+        expect(val).toBe([1, 0]);
+        done();
+    })
+});
