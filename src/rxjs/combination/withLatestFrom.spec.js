d71c184bae2ab0c15a4a930ef8e902b9fe1e09c7
Alex Zagorodnik <olexandr.zagorodnik@gmail.com>
2021-03-11 11:53:54 +0200

    add withLatestFrom test

--- src/rxjs/combination/withLatestFrom.spec.js
+++ src/rxjs/combination/withLatestFrom.spec.js
@@ -1,5 +1,5 @@
-const { withLatestFrom, take } = require('rxjs/operators');
-const { interval } = require('rxjs');
+const { withLatestFrom, take, map } = require('rxjs/operators');
+const { interval, BehaviorSubject, Subject } = require('rxjs');
 
 const example = interval(250).pipe(
     take(3),
@@ -8,7 +8,23 @@ const example = interval(250).pipe(
 
 test('test withLatestFrom', done => {
     example.subscribe(val => {
-        expect(val).toStrictEqual([1, 0]);
+        expect(val).toStrictEqual([1, 0],);
         done();
     })
+});
+
+const bhvSubj1 = new BehaviorSubject('value1');
+const subj2 = new Subject();
+
+const myObservable = subj2.pipe(withLatestFrom(bhvSubj1), map(x => x[1]));
+
+test('test withLatestFrom with behavior subjects', done => {
+    console.log(1);
+    myObservable.subscribe((value) => {
+        console.log(3);
+        expect(value).toStrictEqual('value1');
+        done();
+    });
+    console.log(2);
+    subj2.next('value22');
 });
